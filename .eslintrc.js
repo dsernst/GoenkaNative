ea3783e4544c37c11c27e7470048caf714922fe4
David Ernst <git@dsernst.com>
2020-01-05 23:56:35 -0800

    Auto sort object keys w/ eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,11 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'sort-keys-fix'],
   rules: {
     'no-alert': ['off'],
     semi: ['error', 'never'],
-    'sort-keys': ['warn'],
+    'sort-keys-fix/sort-keys-fix': 'warn',
     'sort-imports': ['warn', { ignoreDeclarationSort: true }],
     'react/jsx-sort-props': ['warn'],
   },
