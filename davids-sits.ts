b109d0ec2a060dfb56c3e577e5093c9e8c55af88
David Ernst <git@dsernst.com>
2020-01-18 01:53:50 -0800

    Clear out initial history

---
+++ davids-sits.ts
@@ -0,0 +1,32 @@
+export default [
+  {
+    date: new Date('Sat Jan 13 2020 9:14'),
+    duration: 15,
+    elapsed: 15,
+  },
+  {
+    date: new Date('Sun Jan 12 2020 22:58'),
+    duration: 45,
+    elapsed: 45,
+  },
+  {
+    date: new Date('Sun Jan 12 2020 12:50'),
+    duration: 5,
+    elapsed: 5,
+  },
+  {
+    date: new Date('Sat Jan 11 2020 11:57'),
+    duration: 60,
+    elapsed: 60,
+  },
+  {
+    date: new Date('Fri Jan 10 2020 22:30'),
+    duration: 10,
+    elapsed: 10,
+  },
+  {
+    date: new Date('Fri Jan 10 2020 8:25'),
+    duration: 35,
+    elapsed: 35,
+  },
+]
--- reducer.ts
+++ reducer.ts
@@ -21,38 +21,7 @@ const initialState: State = {
   finished: false,
   hasChanting: true,
   hasExtendedMetta: false,
-  history: [
-    {
-      date: new Date('Sat Jan 13 2020 9:14'),
-      duration: 15,
-      elapsed: 15,
-    },
-    {
-      date: new Date('Sun Jan 12 2020 22:58'),
-      duration: 45,
-      elapsed: 45,
-    },
-    {
-      date: new Date('Sun Jan 12 2020 12:50'),
-      duration: 5,
-      elapsed: 5,
-    },
-    {
-      date: new Date('Sat Jan 11 2020 11:57'),
-      duration: 60,
-      elapsed: 60,
-    },
-    {
-      date: new Date('Fri Jan 10 2020 22:30'),
-      duration: 10,
-      elapsed: 10,
-    },
-    {
-      date: new Date('Fri Jan 10 2020 8:25'),
-      duration: 35,
-      elapsed: 35,
-    },
-  ],
+  history: [],
   isEnoughTime: true,
   latestTrack: null,
   screen: 'InitScreen',
