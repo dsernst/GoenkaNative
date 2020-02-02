7ce08fed6d13007fe6b0dbc38945241e091b78c8
David Ernst <git@dsernst.com>
2020-02-01 16:39:51 -0800

    Pull MoreInfo section out into its own file

---
+++ src/SettingsScreen/MoreInfo.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Linking, Text, View } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+export default () => (
+  <View style={{ flexDirection: 'row', marginRight: 30, marginTop: 90 }}>
+    <Ionicons
+      color="#fff8"
+      name="ios-information-circle-outline"
+      size={24}
+      style={{ marginLeft: -2, marginTop: 9, paddingRight: 17 }}
+    />
+    <View>
+      <Text style={{ color: '#fffa', fontSize: 16, lineHeight: 27 }}>
+        GoenkaTimer is available for both{'\n'}
+        <Link url="https://apps.apple.com/us/app/id1494609891">
+          <Ionicons name="logo-apple" size={27} />
+          &nbsp; iOS&nbsp;
+        </Link>{' '}
+        and{' '}
+        <Link url="https://play.google.com/store/apps/details?id=com.goenkanative">
+          &nbsp;&nbsp;
+          <Ionicons name="logo-android" size={27} />
+          &nbsp; Android
+        </Link>
+        .
+      </Text>
+      <Text style={{ color: '#fffa', fontSize: 16, lineHeight: 27, marginTop: 15 }}>
+        For more info about S.N. Goenka or Vipassana meditation, visit{' '}
+        <Link url="https://www.dhamma.org">dhamma.org</Link>.
+      </Text>
+    </View>
+  </View>
+)
+
+const Link = ({ url, ...otherProps }: { children: any; url: string }) => (
+  <Text
+    style={{ color: '#0070c9' }}
+    {...otherProps}
+    onPress={() => {
+      Linking.openURL(url)
+    }}
+  />
+)
--- src/SettingsScreen/SettingsScreen.tsx
+++ src/SettingsScreen/SettingsScreen.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import { Linking, Text, TouchableOpacity, View } from 'react-native'
-import Ionicons from 'react-native-vector-icons/Ionicons'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 import { version } from '../../package.json'
 import { Props } from '../reducer'
 import DailyNotificationSettings from './DailyNotifications'
+import MoreInfoSection from './MoreInfo'
 
 class SettingsScreen extends Component<Props> {
   render() {
@@ -39,34 +39,7 @@ class SettingsScreen extends Component<Props> {
         <DailyNotificationSettings {...this.props} />
 
         {/* More Info section */}
-        <View style={{ flexDirection: 'row', marginRight: 30, marginTop: 90 }}>
-          <Ionicons
-            color="#fff8"
-            name="ios-information-circle-outline"
-            size={24}
-            style={{ marginLeft: -2, marginTop: 9, paddingRight: 17 }}
-          />
-          <View>
-            <Text style={{ color: '#fffa', fontSize: 16, lineHeight: 27 }}>
-              GoenkaTimer is available for both{'\n'}
-              <Link url="https://apps.apple.com/us/app/id1494609891">
-                <Ionicons name="logo-apple" size={27} />
-                &nbsp; iOS&nbsp;
-              </Link>{' '}
-              and{' '}
-              <Link url="https://play.google.com/store/apps/details?id=com.goenkanative">
-                &nbsp;&nbsp;
-                <Ionicons name="logo-android" size={27} />
-                &nbsp; Android
-              </Link>
-              .
-            </Text>
-            <Text style={{ color: '#fffa', fontSize: 16, lineHeight: 27, marginTop: 15 }}>
-              For more info about S.N. Goenka or Vipassana meditation, visit{' '}
-              <Link url="https://www.dhamma.org">dhamma.org</Link>.
-            </Text>
-          </View>
-        </View>
+        <MoreInfoSection />
 
         {/* Back button */}
         <TouchableOpacity
@@ -85,15 +58,5 @@ class SettingsScreen extends Component<Props> {
     )
   }
 }
-
-const Link = ({ url, ...otherProps }: { children: any; url: string }) => (
-  <Text
-    style={{ color: '#0070c9' }}
-    {...otherProps}
-    onPress={() => {
-      Linking.openURL(url)
-    }}
-  />
-)
 
 export default SettingsScreen
