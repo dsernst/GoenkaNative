a80e362e18d2e5e63ecb4b21ca423845009db0af
David Ernst <git@dsernst.com>
2020-02-02 00:32:55 -0800

    Pull HistoryScreen CurrentStreaks logic out into its own file

---
+++ src/HistoryScreen/CurrentStreaks.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import { Props } from '../reducer'
+import calcStreak from './calc-streak'
+
+export default (props: Props) => {
+  const { history } = props
+  const dates = history.map(h => h.date)
+  const dailyStreak = calcStreak(dates)
+  const twiceADayStreak = calcStreak(dates, 2)
+
+  return (
+    <Text
+      style={{
+        color: '#fffe',
+        fontWeight: '600',
+        lineHeight: 21,
+        marginBottom: 30,
+        textAlign: 'center',
+      }}
+    >
+      <Faded>You've sat twice a day for </Faded>
+      {twiceADayStreak} day
+      {twiceADayStreak === 1 ? '' : 's'}
+      <Faded>,</Faded>
+      {'\n'}
+      <Faded>& at least once for </Faded>
+      {dailyStreak} day
+      {dailyStreak === 1 ? '' : 's'} straight<Faded>.</Faded>
+    </Text>
+  )
+}
+
+const Faded = (props: any) => <Text {...props} style={{ color: '#fff8', fontWeight: '400', ...props.style }} />
--- src/HistoryScreen/HistoryScreen.tsx
+++ src/HistoryScreen/HistoryScreen.tsx
@@ -1,40 +1,17 @@
 import React from 'react'
-import { Text } from 'react-native'
 
 import BackButton from '../BackButton'
 import { Props } from '../reducer'
 import TitleBar from '../TitleBar'
-import calcStreak from './calc-streak'
+import CurrentStreaks from './CurrentStreaks'
 import ListView from './ListView'
 
 export default (props: Props) => {
-  const { history } = props
-  const dates = history.map(h => h.date)
-  const dailyStreak = calcStreak(dates)
-  const twiceADayStreak = calcStreak(dates, 2)
-
   return (
     <>
       <TitleBar name="HISTORY" style={{ marginHorizontal: 17 }} />
 
-      <Text
-        style={{
-          color: '#fffe',
-          fontWeight: '600',
-          lineHeight: 21,
-          marginBottom: 30,
-          textAlign: 'center',
-        }}
-      >
-        <Faded>You've sat twice a day for </Faded>
-        {twiceADayStreak} day
-        {twiceADayStreak === 1 ? '' : 's'}
-        <Faded>,</Faded>
-        {'\n'}
-        <Faded>& at least once for </Faded>
-        {dailyStreak} day
-        {dailyStreak === 1 ? '' : 's'} straight<Faded>.</Faded>
-      </Text>
+      <CurrentStreaks {...props} />
 
       <ListView {...props} />
 
@@ -42,5 +19,3 @@ export default (props: Props) => {
     </>
   )
 }
-
-const Faded = (props: any) => <Text {...props} style={{ color: '#fff8', fontWeight: '400', ...props.style }} />
