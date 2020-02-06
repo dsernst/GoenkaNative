36f2f0641f0a095812d57afed45a21e275dbea88
David Ernst <git@dsernst.com>
2020-02-06 04:31:44 -0800

    Hide 'Logout' function behind phoneNumber.onLongPress

--- src/SettingsScreen/SyncScreen/LoggedIn.tsx
+++ src/SettingsScreen/SyncScreen/LoggedIn.tsx
@@ -1,6 +1,6 @@
 import auth from '@react-native-firebase/auth'
 import React from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Alert, Text, TouchableOpacity, View } from 'react-native'
 import Octicons from 'react-native-vector-icons/Octicons'
 
 import { Props } from '../../reducer'
@@ -10,19 +10,25 @@ const EnterPhone = ({ history, user }: Props) => {
     <>
       {/* Login Info */}
       <View style={{ alignItems: 'center', flexDirection: 'row', marginTop: 30 }}>
-        <Text style={{ color: '#fff9' }}>
-          Verified as:&nbsp;&nbsp;&nbsp;
+        <Text style={{ color: '#fff9' }}>Verified as:</Text>
+        <TouchableOpacity
+          onLongPress={() =>
+            Alert.alert('Would you like to logout?', '', [
+              { text: 'Cancel' },
+              {
+                onPress: async () => {
+                  await auth().signOut()
+                },
+                style: 'destructive',
+                text: 'Logout',
+              },
+            ])
+          }
+          style={{ padding: 10, paddingRight: 0 }}
+        >
           <Text style={{ alignSelf: 'center', color: '#fffd', fontStyle: 'italic', fontWeight: '500' }}>
             {prettyPhoneNumber(user!.phoneNumber!)}
           </Text>
-        </Text>
-        <TouchableOpacity
-          onPress={async () => {
-            await auth().signOut()
-          }}
-          style={{ marginLeft: 'auto', padding: 10, paddingRight: 0 }}
-        >
-          <Text style={{ color: '#c7dcff' }}>( Logout )</Text>
         </TouchableOpacity>
       </View>
 
