af8d6db5e7ef28c134c9e40460c78eae524ab84d
David Ernst <git@dsernst.com>
2020-02-20 12:58:03 -0800

    Line up MoreInfoSection w/ the rest of SettingsScreen

--- src/SettingsScreen/MoreInfo.tsx
+++ src/SettingsScreen/MoreInfo.tsx
@@ -3,16 +3,11 @@ import { Linking, Text, View } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
 export default () => (
-  <View style={{ flexDirection: 'row', marginRight: 30 }}>
-    <Ionicons
-      color="#fff8"
-      name="ios-information-circle-outline"
-      size={24}
-      style={{ marginTop: 9, paddingRight: 15 }}
-    />
+  <View style={{ flexDirection: 'row', marginLeft: 19, marginRight: 40, marginTop: 30 }}>
+    <Ionicons color="#fff8" name="ios-information-circle-outline" size={24} style={{ marginRight: 14, top: 9 }} />
     <View>
       <Text style={{ color: '#fffa', fontSize: 16, lineHeight: 27 }}>
-        For more info about S.N. Goenka or Vipassana meditation, visit{' '}
+        For more information about S.N. Goenka or Vipassana meditation, visit{' '}
         <Link url="https://www.dhamma.org">dhamma.org</Link>.
       </Text>
       <Text style={{ color: '#fffa', fontSize: 16, lineHeight: 27, marginTop: 30, opacity: 1 }}>
