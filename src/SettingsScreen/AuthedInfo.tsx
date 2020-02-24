a428921518fd010bd274cc0baf015590eef26987
David Ernst <git@dsernst.com>
2020-02-23 18:01:01 -0800

    Simplify AuthedInfo UI

--- src/SettingsScreen/AuthedInfo.tsx
+++ src/SettingsScreen/AuthedInfo.tsx
@@ -1,7 +1,7 @@
 import { FirebaseAuthTypes } from '@react-native-firebase/auth'
 import auth from '@react-native-firebase/auth'
 import React from 'react'
-import { Alert, Text, TouchableOpacity, View } from 'react-native'
+import { Alert, AlertButton, Text, TouchableOpacity, View } from 'react-native'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 
 import { Props } from '../reducer'
@@ -10,40 +10,41 @@ import { prettyDisplayPhone } from './Friends/phone-helpers'
 type AuthInfoProps = Props & { user: FirebaseAuthTypes.User }
 
 function AuthedInfo({ displayName, setState, user }: AuthInfoProps) {
+  const alertButtons: AlertButton[] = displayName
+    ? [
+        {
+          onPress: async () => setState({ displayName: null }),
+          text: 'Reset Display Name',
+        },
+      ]
+    : []
+
+  alertButtons.push(
+    {
+      onPress: () => auth().signOut(),
+      style: 'destructive',
+      text: 'Logout',
+    },
+    { style: 'cancel', text: 'Cancel' },
+  )
+
   return (
     <View style={{ alignItems: 'center', flexDirection: 'row', left: 17, marginBottom: 30, marginTop: 10 }}>
       <AntDesign color="#fff8" name="checkcircle" size={19} style={{ marginRight: 15, top: 1 }} />
       <TouchableOpacity
         onPress={() =>
-          Alert.alert('What would you like to do?', '', [
-            { style: 'cancel', text: 'Cancel' },
-            {
-              onPress: async () => setState({ displayName: null }),
-              style: 'destructive',
-              text: 'Reset Display Name',
-            },
-            {
-              onPress: () => auth().signOut(),
-              style: 'destructive',
-              text: 'Logout',
-            },
-          ])
+          Alert.alert(
+            'Logged in as:',
+            `${displayName ? displayName + ' ' : ''}${prettyDisplayPhone(user.phoneNumber!)}`,
+            alertButtons,
+          )
         }
         style={{ alignItems: 'center', flexDirection: 'row' }}
       >
         <Text style={{ color: '#fff9', marginRight: 15 }}>Logged in as:</Text>
-        {displayName ? (
-          <View style={{ marginBottom: 10, top: 15 }}>
-            <Text style={{ color: '#fffb', fontWeight: '500' }}>{displayName}</Text>
-            <Text style={{ color: '#fff7', fontStyle: 'italic', marginTop: 2 }}>
-              {prettyDisplayPhone(user.phoneNumber!)}
-            </Text>
-          </View>
-        ) : (
-          <Text style={{ color: '#fffb', fontStyle: 'italic', fontWeight: '500' }}>
-            {prettyDisplayPhone(user.phoneNumber!)}
-          </Text>
-        )}
+        <Text style={{ color: '#fffb', fontStyle: displayName ? 'normal' : 'italic', fontWeight: '500' }}>
+          {displayName ? displayName : prettyDisplayPhone(user.phoneNumber!)}
+        </Text>
       </TouchableOpacity>
     </View>
   )
