f81a5965528a93b3b491e433f629bf09801cc396
David Ernst <git@dsernst.com>
2020-02-23 20:12:13 -0800

    Use displayName in Accepted Friends list

--- src/SettingsScreen/Friends/AcceptedRequests.tsx
+++ src/SettingsScreen/Friends/AcceptedRequests.tsx
@@ -16,52 +16,63 @@ function AcceptedRequests({
     <>
       <Text style={{ color: '#fffa', fontWeight: '600', marginTop: 30 }}>Friends:</Text>
       {acceptedIncomingFriendRequests?.map(request => (
-        <View key={request.id} style={{ alignItems: 'center', flexDirection: 'row', marginTop: 15 }}>
+        <View key={request.id} style={{ flexDirection: 'row', marginTop: 15 }}>
           <TouchableOpacity
             onPress={() =>
-              Alert.alert('Are you sure?', 'This will stop them from seeing your notifications as well.', [
-                { text: 'Cancel' },
-                {
-                  onPress: () =>
-                    firestore()
-                      .collection('friendRequests')
-                      .doc(request.id)
-                      .update({
-                        accepted: firestore.FieldValue.delete(),
-                        rejected: new Date(),
-                      }),
-                  style: 'destructive',
-                  text: 'Delete',
-                },
-              ])
+              Alert.alert(
+                `Unfriend ${request.from_name}?`,
+                'Are you sure? This will stop them from seeing your notifications as well.',
+                [
+                  { text: 'Cancel' },
+                  {
+                    onPress: () =>
+                      firestore()
+                        .collection('friendRequests')
+                        .doc(request.id)
+                        .delete(),
+                    style: 'destructive',
+                    text: 'Unfriend',
+                  },
+                ],
+              )
             }
           >
             <Text style={{ color: '#ff5e5eee' }}>&nbsp; ✗&nbsp; </Text>
           </TouchableOpacity>
-          <Text style={{ color: '#fffa' }}>&nbsp; {prettyDisplayPhone(request.from_phone)}</Text>
+          <View style={{ marginLeft: 5 }}>
+            <Text style={{ color: '#fffa' }}>&nbsp; {request.from_name}</Text>
+            <Text style={{ color: '#fff5' }}>&nbsp; {prettyDisplayPhone(request.from_phone)}</Text>
+          </View>
         </View>
       ))}
       {acceptedOutgoingFriendRequests?.map(request => (
-        <View key={request.id} style={{ alignItems: 'center', flexDirection: 'row', marginTop: 15 }}>
+        <View key={request.id} style={{ flexDirection: 'row', marginTop: 15 }}>
           <TouchableOpacity
             onPress={() =>
-              Alert.alert('Are you sure?', 'This will stop them from seeing your notifications as well.', [
-                { text: 'Cancel' },
-                {
-                  onPress: () =>
-                    firestore()
-                      .collection('friendRequests')
-                      .doc(request.id)
-                      .delete(),
-                  style: 'destructive',
-                  text: 'Delete',
-                },
-              ])
+              Alert.alert(
+                `Unfriend ${request.to_name}?`,
+                'Are you sure? This will stop them from seeing your notifications as well.',
+                [
+                  { text: 'Cancel' },
+                  {
+                    onPress: () =>
+                      firestore()
+                        .collection('friendRequests')
+                        .doc(request.id)
+                        .delete(),
+                    style: 'destructive',
+                    text: 'Unfriend',
+                  },
+                ],
+              )
             }
           >
             <Text style={{ color: '#ff5e5eee' }}>&nbsp; ✗&nbsp; </Text>
           </TouchableOpacity>
-          <Text style={{ color: '#fffa' }}>&nbsp; {prettyDisplayPhone(request.to_phone)}</Text>
+          <View style={{ marginLeft: 5 }}>
+            <Text style={{ color: '#fffa' }}>&nbsp; {request.to_name}</Text>
+            <Text style={{ color: '#fff5' }}>&nbsp; {prettyDisplayPhone(request.to_phone)}</Text>
+          </View>
         </View>
       ))}
     </>
