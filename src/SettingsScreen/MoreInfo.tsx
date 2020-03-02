5db1a9755fefe7a7a9d85b3de94ab3cd72044de2
David Ernst <git@dsernst.com>
2020-03-02 14:42:46 -0800

    Fix Settings.MoreInfo cutoff on the bottom

--- src/SettingsScreen/MoreInfo.tsx
+++ src/SettingsScreen/MoreInfo.tsx
@@ -3,14 +3,14 @@ import { Linking, Text, View } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
 export default () => (
-  <View style={{ flexDirection: 'row', marginLeft: 19, marginRight: 40, marginTop: 30 }}>
+  <View style={{ flexDirection: 'row', marginLeft: 19, marginRight: 40, marginVertical: 30 }}>
     <Ionicons color="#fff8" name="ios-information-circle-outline" size={24} style={{ marginRight: 14, top: 9 }} />
     <View>
       <Text style={{ color: '#fffa', fontSize: 16, lineHeight: 27 }}>
         For more information about S.N. Goenka or Vipassana meditation, visit{' '}
         <Link url="https://www.dhamma.org">dhamma.org</Link>.
       </Text>
-      <Text style={{ color: '#fffa', fontSize: 16, lineHeight: 27, marginTop: 30, opacity: 1 }}>
+      <Text style={{ color: '#fffa', fontSize: 16, lineHeight: 27, marginTop: 30 }}>
         For help/questions: <Link url="mailto:hi@goenka.app">hi@goenka.app</Link>
       </Text>
     </View>
