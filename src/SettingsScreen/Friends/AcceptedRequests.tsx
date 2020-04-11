442edc1521207ff1e5b575f80bb4055563b83985
David Ernst <git@dsernst.com>
2020-04-10 23:38:23 -0700

    Sort friends list alphabetically

--- src/SettingsScreen/Friends/AcceptedRequests.tsx
+++ src/SettingsScreen/Friends/AcceptedRequests.tsx
@@ -12,82 +12,80 @@ function AcceptedRequests({
   acceptedIncomingFriendRequests: FriendRequest[]
   acceptedOutgoingFriendRequests: FriendRequest[]
 }) {
-  // Unique values for incoming vs outgoing friend requests
-  const tuple: [
-    FriendRequest[],
-    (request: FriendRequest) => string,
-    (request: FriendRequest) => string,
-    (request: FriendRequest) => boolean,
-    (request: FriendRequest) => object,
-  ][] = [
-    [
-      acceptedIncomingFriendRequests,
-      request => request.from_name,
-      request => request.from_phone,
-      request => request.to_wants_notifs,
-      request => ({ to_wants_notifs: !request.to_wants_notifs }),
-    ],
-    [
-      acceptedOutgoingFriendRequests,
-      request => request.to_name,
-      request => request.to_phone,
-      request => request.from_wants_notifs,
-      request => ({ from_wants_notifs: !request.from_wants_notifs }),
-    ],
-  ]
+  // Merge incoming & outgoing friend requests
+  const allRequests: {
+    name: string
+    phone: string
+    request: FriendRequest
+    toggle_notifs: object
+    wants_notifs: boolean
+  }[] = [
+    ...acceptedIncomingFriendRequests.map(iFR => ({
+      name: iFR.from_name,
+      phone: iFR.from_phone,
+      request: iFR,
+      toggle_notifs: { to_wants_notifs: !iFR.to_wants_notifs },
+      wants_notifs: iFR.to_wants_notifs,
+    })),
+    ...acceptedOutgoingFriendRequests.map(oFR => ({
+      name: oFR.to_name,
+      phone: oFR.to_phone,
+      request: oFR,
+      toggle_notifs: { from_wants_notifs: !oFR.from_wants_notifs },
+      wants_notifs: oFR.from_wants_notifs,
+    })),
+  ].sort((a, b) => new Intl.Collator().compare(a.name, b.name))
 
   return (
     <>
       <Text style={{ color: '#fff4', fontWeight: '600', marginTop: 30 }}>Accepted:</Text>
-      {tuple.map(([requests, getName, getPhone, getNotifs, toggleNotifs]) =>
-        requests?.map(request => (
-          <View key={request.id} style={{ flexDirection: 'row', marginTop: 15, opacity: getNotifs(request) ? 1 : 0.3 }}>
-            <TouchableOpacity
-              onPress={() =>
-                Alert.alert(
-                  `Unfriend ${getName(request)}?`,
-                  'Are you sure? This will stop them from seeing your notifications as well.',
-                  [
-                    { text: 'Cancel' },
-                    {
-                      onPress: () =>
-                        firestore()
-                          .collection('friendRequests')
-                          .doc(request.id)
-                          .delete(),
-                      style: 'destructive',
-                      text: 'Unfriend',
-                    },
-                  ],
-                )
-              }
-            >
-              <Text style={{ color: '#ff5e5eee' }}>&nbsp; ✗&nbsp; </Text>
-            </TouchableOpacity>
-            <View style={{ marginLeft: 5 }}>
-              <Text style={{ color: '#fffb' }}>&nbsp; {getName(request)}</Text>
-              <Text style={{ color: '#fff5' }}>&nbsp; {prettyDisplayPhone(getPhone(request))}</Text>
-            </View>
-            <Switch
-              onValueChange={() =>
-                firestore()
-                  .collection('friendRequests')
-                  .doc(request.id)
-                  .update(toggleNotifs(request))
-              }
-              style={{
-                alignSelf: 'flex-end',
-                marginLeft: 'auto',
-                paddingVertical: 10,
-                transform: Platform.OS === 'ios' ? [{ scaleX: 0.8 }, { scaleY: 0.8 }] : [],
-              }}
-              thumbColor="white"
-              trackColor={{ false: 'null', true: 'rgb(10, 132, 255)' }}
-              value={getNotifs(request)}
-            />
+      {allRequests.map(({ name, phone, request, toggle_notifs, wants_notifs }) => (
+        <View key={request.id} style={{ flexDirection: 'row', marginTop: 15, opacity: wants_notifs ? 1 : 0.3 }}>
+          <TouchableOpacity
+            onPress={() =>
+              Alert.alert(
+                `Unfriend ${name}?`,
+                'Are you sure? This will stop them from seeing your notifications as well.',
+                [
+                  { text: 'Cancel' },
+                  {
+                    onPress: () =>
+                      firestore()
+                        .collection('friendRequests')
+                        .doc(request.id)
+                        .delete(),
+                    style: 'destructive',
+                    text: 'Unfriend',
+                  },
+                ],
+              )
+            }
+          >
+            <Text style={{ color: '#ff5e5eee' }}>&nbsp; ✗&nbsp; </Text>
+          </TouchableOpacity>
+          <View style={{ marginLeft: 5 }}>
+            <Text style={{ color: '#fffb' }}>&nbsp; {name}</Text>
+            <Text style={{ color: '#fff5' }}>&nbsp; {prettyDisplayPhone(phone)}</Text>
           </View>
-        )),
-      )}
+          <Switch
+            onValueChange={() =>
+              firestore()
+                .collection('friendRequests')
+                .doc(request.id)
+                .update(toggle_notifs)
+            }
+            style={{
+              alignSelf: 'flex-end',
+              marginLeft: 'auto',
+              paddingVertical: 10,
+              transform: Platform.OS === 'ios' ? [{ scaleX: 0.8 }, { scaleY: 0.8 }] : [],
+            }}
+            thumbColor="white"
+            trackColor={{ false: 'null', true: 'rgb(10, 132, 255)' }}
+            value={wants_notifs}
+          />
+        </View>
+      ))}
     </>
   )
 }
