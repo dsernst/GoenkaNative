0a9b2b427cf39409c7eec07f12cc8fb43eaa5c5d
David Ernst <git@dsernst.com>
2020-04-11 23:57:46 -0700

    SettingsScreen: Improve the look of section descriptions

--- src/SettingsScreen/Section.tsx
+++ src/SettingsScreen/Section.tsx
@@ -30,55 +30,85 @@ function Section(props: SectionProps) {
 
   return (
     <View style={{ marginBottom: enabled ? 50 : 30 }}>
-      <TouchableOpacity
-        activeOpacity={0.7}
-        onPress={() => setEnabled(!enabled)}
-        style={{
-          alignItems: 'center',
-          alignSelf: 'center',
-          borderColor: enabled ? '#9CDCFE55' : '#fff1',
-          borderRadius: 8,
-          borderWidth: 1,
-          flexDirection: 'row',
-          height: 38,
-          marginBottom: enabled ? 20 : 0,
-          paddingHorizontal: 15,
-          width: '100%',
-        }}
-      >
-        <View style={{ alignItems: 'center', marginRight: 10, width: 25 }}>
-          <Set color={enabled ? '#fffd' : '#fff8'} {...icon} style={{ paddingTop: 2 }} />
-        </View>
-        <Text style={{ color: enabled ? '#fffd' : '#fff8', fontSize: 16 }}>{title}</Text>
+      {/* Section header clickable */}
+      <TouchableOpacity activeOpacity={0.7} onPress={() => setEnabled(!enabled)}>
+        <View
+          style={{
+            alignItems: 'center',
+            alignSelf: 'center',
+            borderColor: enabled ? '#9CDCFE55' : '#fff1',
+            borderRadius: 8,
+            borderWidth: 1,
+            flexDirection: 'row',
+            height: 38,
+            paddingHorizontal: 15,
+            width: '100%',
+          }}
+        >
+          {/* Section icon */}
+          <View style={{ alignItems: 'center', marginRight: 10, width: 25 }}>
+            <Set color={enabled ? '#fffd' : '#fff8'} {...icon} style={{ paddingTop: 2 }} />
+          </View>
 
-        {!!badgeNumber && (
+          {/* Section title */}
+          <Text style={{ color: enabled ? '#fffd' : '#fff8', fontSize: 16 }}>{title}</Text>
+
+          {/* Notification badges */}
+          {!!badgeNumber && (
+            <View
+              style={{
+                backgroundColor: `#f8ff70${enabled ? '' : 'bb'}`,
+                borderRadius: 30,
+                marginLeft: 15,
+                paddingHorizontal: 5,
+              }}
+            >
+              <Text style={{ color: '#000', fontWeight: '700' }}>{badgeNumber}</Text>
+            </View>
+          )}
+
+          {/* Expand / expanded arrow */}
+          <Octicons
+            color="#fff5"
+            name={enabled ? 'chevron-down' : 'chevron-right'}
+            size={18}
+            style={{ marginLeft: 'auto', paddingTop: 3 }}
+          />
+        </View>
+
+        {/* Description */}
+        {enabled && description && (
           <View
             style={{
-              backgroundColor: `#f8ff70${enabled ? '' : 'bb'}`,
-              borderRadius: 30,
-              marginLeft: 15,
-              paddingHorizontal: 5,
+              backgroundColor: '#9CDCFE11',
+              borderBottomLeftRadius: 15,
+              borderBottomRightRadius: 15,
+              borderColor: '#9CDCFE19',
+              borderTopWidth: 0,
+              borderWidth: 1,
+              flex: 1,
+              marginHorizontal: 10,
             }}
           >
-            <Text style={{ color: '#000', fontWeight: '700' }}>{badgeNumber}</Text>
+            <Text
+              style={{
+                bottom: 1,
+                color: '#fff6',
+                fontWeight: '400',
+                paddingLeft: 10,
+                paddingVertical: 5,
+              }}
+            >
+              {description}
+            </Text>
           </View>
         )}
-
-        <Octicons
-          color="#fff5"
-          name={enabled ? 'chevron-down' : 'chevron-right'}
-          size={18}
-          style={{ marginLeft: 'auto', paddingTop: 3 }}
-        />
       </TouchableOpacity>
-      <View style={{ paddingHorizontal: 18 }}>
-        {enabled && (
-          <>
-            {description && <Text style={{ color: '#fffa', fontWeight: '600', marginBottom: 30 }}>{description}</Text>}
-            {requiresLogin && !user ? <LoginFlow {...props} /> : <Content {...props} />}
-          </>
-        )}
-      </View>
+      {enabled && (
+        <View style={{ marginTop: 30, paddingHorizontal: 18 }}>
+          {requiresLogin && !user ? <LoginFlow {...props} /> : <Content {...props} />}
+        </View>
+      )}
     </View>
   )
 }
