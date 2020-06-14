57332af4019d386b6d637c7e0fc4721af2d2bcbe
David Ernst <git@dsernst.com>
2020-06-14 11:09:54 -0700

    Tweak look of MainScreen tab bar

--- src/MainScreen/TypeSwitcher.tsx
+++ src/MainScreen/TypeSwitcher.tsx
@@ -6,8 +6,8 @@ const backgroundColor = '#001709'
 export default ({ selectView, viewIndex }: { selectView: (index: number) => void; viewIndex: number }) => (
   <View
     style={{
-      backgroundColor: '#40514710',
-      borderColor: '#40514748',
+      backgroundColor: '#40514705',
+      borderColor: '#40514710',
       borderTopWidth: 1,
       borderWidth: 0,
       flexDirection: 'row',
@@ -20,11 +20,14 @@ export default ({ selectView, viewIndex }: { selectView: (index: number) => void
         activeOpacity={1}
         key={label}
         onPress={() => selectView(index)}
-        style={[index === viewIndex && { backgroundColor }]}
+        style={[
+          { borderColor: '#0000', borderTopWidth: 1, bottom: 1 },
+          index === viewIndex && { backgroundColor, borderColor: '#40514760' },
+        ]}
       >
         <Text
           style={{
-            color: index === viewIndex ? '#fff7' : '#fff3',
+            color: index === viewIndex ? '#fff7' : '#fff2',
             fontSize: 13,
             paddingHorizontal: 24,
             paddingVertical: 6,
